1545382c4bd4dddb944d460d5a9d7d1c998b8caa
Mark Lourenco <markjlourencor@gmail.com>
2025-04-12 02:42:21 -0700

    Made New Weapons

--- Assets/Scrips/Weapon/WeaponBullet.cs
+++ Assets/Scrips/Weapon/WeaponBullet.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -7,7 +8,12 @@ public class WeaponBullet : MonoBehaviour
     public int damage = 10;
     public float lifeTime = 2.5f;
 
+    [Header("Piercing Ammo")]
+    public bool piercingAmmo = false;
+    public float hitCooldown = 1.0f;
+
     private Vector2 direction;
+    private Dictionary<Collider2D, float> enemyHitTimestamps = new Dictionary<Collider2D, float>();
 
     private void Start()
     {
@@ -17,6 +23,19 @@ public class WeaponBullet : MonoBehaviour
     private void Update()
     {
         transform.position += (Vector3)(direction * speed * Time.deltaTime);
+
+        List<Collider2D> toRemove = new List<Collider2D>();
+        foreach (var pair in enemyHitTimestamps)
+        {
+            if (Time.time - pair.Value >= hitCooldown)
+            {
+                toRemove.Add(pair.Key);
+            }
+        }
+        foreach (var enemy in toRemove)
+        {
+            enemyHitTimestamps.Remove(enemy);
+        }
     }
 
     public void SetDirection(Vector2 dir)
@@ -30,9 +49,19 @@ public class WeaponBullet : MonoBehaviour
     {
         if (collision.CompareTag("Enemy"))
         {
+            if (enemyHitTimestamps.ContainsKey(collision))
+            {
+                return;
+            }
+
             Debug.Log("Hit enemy");
             collision.GetComponent<Enemy>()?.TakeDamage(damage);
-            Destroy(gameObject);
+            enemyHitTimestamps[collision] = Time.time;
+
+            if (!piercingAmmo)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
